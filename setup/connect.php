4d86aebd61bcc232101e22843794dc29cf1a836f
aghoghojoy <joy@loyaltysolutionsnigeria.com>
2019-06-15 16:37:27 +0100

    essage=Added Connection String

---
+++ setup/connect.php
@@ -0,0 +1,5 @@
+<?php
+
+$connect = mysqli_connect('localhost', 'root', 'root', 'rims_live2') or trigger_error(mysqli_error(), E_USER_ERROR);
+
+?>
---
+++ setup/connect_heroku.php
@@ -0,0 +1,10 @@
+<?php
+
+$connect = mysqli_connect(
+    'q7cxv1zwcdlw7699.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+    'vrqv9ft8g5uve89n',
+    'szfvyrrj1mpi11ay',
+    'zvnvlxg8q4k4kfoh',
+    3306)
+    or trigger_error(mysqli_error(), E_USER_ERROR);
+
