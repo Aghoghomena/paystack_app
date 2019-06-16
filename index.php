874d81698d6d98b2ac9ed50e161de6edfd12d517
aghoghojoy <joy@loyaltysolutionsnigeria.com>
2019-06-16 11:59:12 +0100

    essage=Added Login Page

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once "./setup/connect_heroku.php";
-
-$get_users ="select email from users";
-$query = mysqli_query($connect,$get_users);
-$row = mysqli_fetch_row($query);
-print_r($row);
-echo "test";
